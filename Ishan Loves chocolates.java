bfb327f723cec7d1490d6ae5972335ce282484a8
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2022-04-10 23:14:34 +0530

    Create Ishan Loves chocolates.java

---
+++ Ishan Loves chocolates.java
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		     
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    Complete obj = new Complete();
+		    int ans = obj.chocolates(arr, sizeOfArray);
+		    System.out.println(ans);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int chocolates (int arr[], int n) {
+        //Complete the function
+        // Arrays.sort(arr);
+        // int c=arr[0];
+        // return c;
+        int temp =arr[0];
+        for(int i=0;i<n;i++){
+            
+            if(arr[i]<temp){
+                temp = arr[i];
+            }
+        }
+        return temp;
+    }
+    
+    
+}
+
+
